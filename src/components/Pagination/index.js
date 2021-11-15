9a20485eb50d3e9644e15d82d93fec0a5b239f97
lnadaigia <72127469+lnadaigia@users.noreply.github.com>
2021-11-15 20:46:51 +0700

    update pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,13 +1,43 @@
 import React from "react";
 import './style.scss'
 const Pagination = () => {
+  function pagination(c, m) {
+    var current = c,
+        last = m,
+        delta = 2,
+        left = current - delta,
+        right = current + delta + 1,
+        range = [],
+        rangeWithDots = [],
+        l;
+
+    for (let i = 1; i <= last; i++) {
+        if (i == 1 || i == last || i >= left && i < right) {
+            range.push(i);
+        }
+    }
+
+    for (let i of range) {
+        if (l) {
+            if (i - l === 2) {
+                rangeWithDots.push(l + 1);
+            } else if (i - l !== 1) {
+                rangeWithDots.push('...');
+            }
+        }
+        rangeWithDots.push(i);
+        l = i;
+    }
+
+    return rangeWithDots;
+}
   return (
     <div class="pagination">
       <a href="#">&laquo;</a>
      {
-         [1,2,3,4,5,6].map((item,index)=>{
+         pagination(6,20).map((item,index)=>{
             return(
-                <a key={index} className={item===2?"active":""} href="#">{item}</a>
+                <a key={index} className={index==0?"active":""} href="#">{item}</a>
             )
          })
      }
