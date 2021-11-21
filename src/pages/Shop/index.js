fe7eb7574319e645423e4a4292efacd89e4cac3f
lnadaigia <72127469+lnadaigia@users.noreply.github.com>
2021-11-21 22:16:38 +0700

    update shop

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,34 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Widget from "./widget";
 import "./style.scss";
 import Product from "../../components/Product";
 import Pagination from "../../components/Pagination";
 const Shop = () => {
+ 
   return (
     <div className="shop">
-      <div className="row content">
-        <div className="col c-3 md-12 ">
-          <Widget />
-        </div>
-        <div className="col c-8 md-12 ">
-          <div className="sort-bar row">
-            <p className="display">Hiển thị 1-12 trong 32 sản phẩm</p>
-            <select name="sort" className="sortby">
-              <option value="">Sắp xếp theo: Mặc định</option>
-              <option value="-createdAt">Sắp xếp theo: Mới nhất</option>
-              <option value="-createdAt">Sắp xếp theo: Cũ nhất</option>
-              <option value="-price">Sắp xếp theo giá: Từ thấp đến cao</option>
-              <option value="price">Sắp xếp theo giá: Từ cao đến thấp</option>
-            </select>
-            <select name="sort" className="sortby">
-              <option value="12">Hiển thị 12</option>
-              <option value="5">Hiển thị 5</option>
-              <option value="20">Hiển thị 20</option>
-            </select>
-          </div>
+     
+      <Widget />
+
           <div className="row">
             {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
-              <div className="col c-4 md-6 sm-12">
+              <div className="col c-3 lg-4 md-6">
                 <Product
                   imageURL={"./images/img1.jpg"}
                   name={
@@ -40,11 +24,11 @@ const Shop = () => {
                 />
               </div>
             ))}
+            <div className="col c-11"> <Pagination /></div>
           </div>
-          <Pagination />
-        </div>
+         
+   
       </div>
-    </div>
   );
 };
 
--- src/pages/Shop/widget/index.js
+++ src/pages/Shop/widget/index.js
@@ -6,54 +6,102 @@ const Widget = () => {
     { name: "Truyện ngắn – Tản văn", description: "" },
     { name: "Range - Hiểu Sâu, Biết Rộng Kiểu Gì Cũng Thắng", description: "" },
   ];
-  const authors = [{ 'name': 'Dương Thụy' }, { 'name': 'Anh Cầm Fact' }, { 'name': 'Trần Minh Phương Thảo' }, { 'name': 'David Epstein' }]
+  const authors = [
+    { name: "Dương Thụy" },
+    { name: "Anh Cầm Fact" },
+    { name: "Trần Minh Phương Thảo" },
+    { name: "David Epstein" },
+  ];
   const [category, setCategory] = useState(true);
   const [author, setAuthor] = useState(true);
 
-  const toggleCategory = () => setCategory(!category);
-  const toggleAuthor = () => setAuthor(!author);
+  const toggleCategory = (e) => {
+    e.stopPropagation();
+    setCategory(!category);
+  };
+  const toggleAuthor = (e) => {
+    e.stopPropagation();
+    setAuthor(!author);
+  };
+  const [sidebar, setSidebar] = useState(true);
+  const showSidebar = () => setSidebar(!sidebar);
   return (
-    <div className="widget">
-      <div className="category">
-        <div className="category__title">
-          <h4>Danh mục</h4>
-          <div className="toggle" onClick={toggleCategory}>
-            {category ? <i class="fas fa-plus"></i> : <i class="fas fa-minus"></i>}
+    <>
+      <div className="sort-bar row">
+        <p className="display">Hiển thị 1-12 trong 32 sản phẩm</p>
+        <select name="sort" className="sortby">
+          <option value="">Sắp xếp theo: Mặc định</option>
+          <option value="-createdAt">Sắp xếp theo: Mới nhất</option>
+          <option value="-createdAt">Sắp xếp theo: Cũ nhất</option>
+          <option value="-price">Sắp xếp theo giá: Từ thấp đến cao</option>
+          <option value="price">Sắp xếp theo giá: Từ cao đến thấp</option>
+        </select>
+        <select name="sort" className="sortby">
+          <option value="12">Hiển thị 12</option>
+          <option value="5">Hiển thị 5</option>
+          <option value="20">Hiển thị 20</option>
+        </select>
+        <span className="filter" onClick={showSidebar}>
+          Bộ lọc <i class="fas fa-filter"></i>
+        </span>
+      </div>
+      <div className={sidebar ? "modal active" : "modal"} onClick={showSidebar}>
+        <div className="widget">
+          <div className="category close">
+            <div className="category__title">
+              <h4>Bộ lọc</h4>
+              <div className="toggle"><i class="fas fa-times"></i></div>
+            </div>
+          </div>
+          <div className="category">
+            <div className="category__title">
+              <h4>Danh mục</h4>
+              <div className="toggle" onClick={toggleCategory}>
+                {category ? (
+                  <i class="fas fa-plus"></i>
+                ) : (
+                  <i class="fas fa-minus"></i>
+                )}
+              </div>
+            </div>
+            {category === true
+              ? categorys.map((item, index) => {
+                  return (
+                    <div className="category__item">
+                      <a key={index} href="#">
+                        {item.name}
+                      </a>
+                    </div>
+                  );
+                })
+              : ""}
+          </div>
+          <div className="category">
+            <div className="category__title">
+              <h4>Tác giả</h4>
+              <div className="toggle" onClick={toggleAuthor}>
+                {author ? (
+                  <i class="fas fa-plus"></i>
+                ) : (
+                  <i class="fas fa-minus"></i>
+                )}
+              </div>
+            </div>
+            {author === true
+              ? authors.map((item, index) => {
+                  return (
+                    <div className="category__item">
+                      <a key={index} href="#">
+                        {item.name}
+                      </a>
+                    </div>
+                  );
+                })
+              : ""}
           </div>
         </div>
-        {
-          category === true ? categorys.map((item, index) => {
-            return (
-              <div className="category__item">
-                <a key={index} href="#">
-                  {item.name}
-                </a>
-              </div>
-            );
-          }) : ""
-        }
       </div>
-      <div className="category">
-        <div className="category__title">
-          <h4>Tác giả</h4>
-          <div className="toggle" onClick={toggleAuthor}>
-            {author ? <i class="fas fa-plus"></i> : <i class="fas fa-minus"></i>}
-          </div>
-        </div>
-        {
-          author === true ? authors.map((item, index) => {
-            return (
-              <div className="category__item">
-                <a key={index} href="#">
-                  {item.name}
-                </a>
-              </div>
-            );
-          }) : ""
-        }
-      </div>
-
-    </div>
+    </>
   );
 };
 
