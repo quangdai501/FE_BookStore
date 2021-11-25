7726a8da5deb0c0e08654ec5d5247b5b4108711f
lnadaigia <72127469+lnadaigia@users.noreply.github.com>
2021-11-25 17:29:07 +0700

    update image

--- src/components/UpLoadImage/index.js
+++ src/components/UpLoadImage/index.js
@@ -1,20 +1,41 @@
-import React, { useRef } from 'react'
-import './style.scss';
-import PropTypes from 'prop-types';
+import React, { useRef, useState } from "react";
+import "./style.scss";
+import PropTypes from "prop-types";
+import { updateImage } from "../../api/updateImage";
 UpLoadImage.propTypes = {
-    setImage: PropTypes.func.isRequired
-}
+  setImage: PropTypes.func.isRequired,
+};
 export default function UpLoadImage(props) {
-    const fileRef = useRef();
-    const handleImage = () => {
-        if (fileRef.current) {
-            console.log(fileRef.current.files[0])
-        }
+  const [img, setImg] = useState("");
+  const fileRef = useRef();
+  const handleImage = () => {
+    if (fileRef.current) {
+      //   console.log(fileRef.current.files[0]);
+      updateImage(fileRef.current.files[0])
+        .then((data) => {
+          console.log(data);
+          if (data.success) {
+            setImg(data.data.url);
+          }
+        })
+        .catch((err) => console.log(err));
     }
+  };
 
-    return (<div className="row upload-img" >
-        <input type="file" ref={fileRef} />
-        <button type="button" onClick={handleImage} className="btn btn--upload btn--border-none">Tải ảnh lên</button>
+  return (
+    <div className="row upload-img">
+      <div > 
+        <img src={img} alt="" />
+      </div>
+
+      <input type="file" ref={fileRef} />
+      <button
+        type="button"
+        onClick={handleImage}
+        className="btn btn--upload btn--border-none"
+      >
+        Tải ảnh lên
+      </button>
     </div>
-    )
+  );
 }
