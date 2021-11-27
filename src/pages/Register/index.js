e9ca778b1d19e6de36599c2fbe449e0f1ccc0ac0
lnadaigia <72127469+lnadaigia@users.noreply.github.com>
2021-11-27 14:42:29 +0700

    update register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { confirmEmail, enterCode } from "../../actions/userAction";
+import {  useNavigate } from "react-router";
+import "./style.scss";
 export default function Register() {
   const [isMatch, setIsMatch] = useState(true);
   const {
@@ -14,9 +18,30 @@ export default function Register() {
     message:
       "Mật khẩu phải chứa ít nhất một số và một chữ cái viết hoa và viết thường và ít nhất 8 ký tự trở lên",
   };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, success, userInfo } = userRegister;
+  const userEnterCode = useSelector((state) => state.userEnterCode);
+  const {
+    loading: loadingEnterCode,
+    error: errorEnterCode,
+    success: successEnterCode,
+    userInfo: userInfoEnterCode,
+  } = userEnterCode;
+
+  useEffect(() => {
+   if(successEnterCode){
+    setTimeout(() => {
+      document.location.href = '/login'
+    }, 2000);
+   }
+  }, [successEnterCode]);
+
+  const [isentercode, setIsentercodes] = useState(false);
   const onSubmit = (data) => {
-    const { passwordAgain, password, email } = data;
-    console.log(passwordAgain, password, email);
+    const { code, name, passwordAgain, password, email } = data;
+    // console.log(passwordAgain, password, email);
     if (password !== passwordAgain) {
       setIsMatch(false);
       return;
@@ -24,7 +49,20 @@ export default function Register() {
       setIsMatch(true);
     }
     if (Object.keys(errors).length === 0) {
+      // console.log(password, email,name);
       //submit form tại đây
+      if (code){
+        if (isentercode) {
+          dispatch(enterCode(code));
+          // console.log(code)
+        } else {
+          // console.log(password, email,name);
+          dispatch(confirmEmail(name, email, password));
+        }
+      }else{
+        dispatch(confirmEmail(name, email, password));
+      }
+     
     }
   };
 
@@ -34,10 +72,21 @@ export default function Register() {
         <div className="login__header">Đăng ký</div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-input">
+            <label htmlFor="name" className="form-label">
+              Họ và tên
+            </label>
+            <input
+              type="name"
+              name="name"
+              {...register("name", { required: true })}
+            />
+          </div>
+          <div className="form-input">
             <label htmlFor="email" className="form-label">
               Email
             </label>
             <input type="email" name="email" {...register("email")} />
+            {error && <p className="error-label">{error}</p>}
           </div>
           <div className="form-input">
             <label htmlFor="password" className="form-label">
@@ -63,9 +112,37 @@ export default function Register() {
             )}
             {!isMatch && <p className="error-label">Mật khẩu không khớp</p>}
           </div>
-          <button type="submit" className="btn btn--border-none btn--full-width">
+          {success && (
+            <div className="form-input">
+              <label htmlFor="code" className="form-label">
+                Mã xác nhận
+              </label>
+              <p className="">Chúng tôi vừa gủi mã xác nhận đến email của bạn</p>
+              {errorEnterCode && (
+                <p className="error-label">{errorEnterCode}</p>
+              )}
+              <input
+                type="code"
+                name="code"
+                {...register("code", { required: true })}
+              />
+              <button
+                type="submit"
+                className="reload-btn"
+                onClick={() => setIsentercodes(false)}
+              >
+                Gủi lại <i class="fas fa-sync"></i>
+              </button>
+            </div>
+          )}
+          <button
+            type="submit"
+            className="btn btn--border-none btn--full-width"
+            onClick={() => setIsentercodes(true)}
+          >
             Đăng ký
           </button>
+          {successEnterCode&&<p className="sucess-register">Đăng ký thành công</p>}
         </form>
         <p className="login__form-panel">
           <Link to="/login">Tôi có tài khoản rồi</Link>
