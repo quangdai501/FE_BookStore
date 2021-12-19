549b4abb50d55408997174043cf7f2053d56ec49
quangdai501 <tranquangdait02@gmail.com>
2021-12-19 22:30:54 +0700

    add modal to order management

--- src/pages/Shop/FilterBar/index.js
+++ src/pages/Shop/FilterBar/index.js
@@ -7,7 +7,7 @@ const FilterBar = (props) => {
       {props.query["author"] ? (
         <div className="label">
           <span>Tác giả: {props.query["author"]}</span>{" "}
-          <b onClick={() => props.removeFilter("author")}>X</b>
+          <b onClick={() => props.removeFilter("author")}><i class="fas fa-times"></i></b>
         </div>
       ) : (
         ""
@@ -15,7 +15,7 @@ const FilterBar = (props) => {
       {props.query["category"] ? (
         <div className="label">
           <span>Danh mục: {props.query["category"]}</span>{" "}
-          <b onClick={() => props.removeFilter("category")}>X</b>
+          <b onClick={() => props.removeFilter("category")}><i class="fas fa-times"></i></b>
         </div>
       ) : (
         ""
@@ -23,7 +23,7 @@ const FilterBar = (props) => {
       {props.query["search"] ? (
         <div className="label">
           <span>Từ khóa: {props.query["search"]}</span>{" "}
-          <b onClick={() => props.removeFilter("search")}>X</b>
+          <b onClick={() => props.removeFilter("search")}><i class="fas fa-times"></i></b>
         </div>
       ) : (
         ""
