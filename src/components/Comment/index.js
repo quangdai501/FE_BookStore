43d57d4e48b5c82c9025c044e99be86615c695ae
vanlinh2505 <72127469+vanlinh2505@users.noreply.github.com>
2022-06-13 22:11:03 +0700

    update comment with user who is purchase product

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,39 +1,61 @@
-import React from 'react'
-import './style.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
 
 export default function Comment(props) {
-    const { name, updatedAt, rating, comment, productName, product } = props.review;
+  const { name, updatedAt, rating, comment, productName, product, isPurchase } =
+    props.review;
 
-    return (
-        <div className="comment">
-            <div className="comment__item">
-                {product ?
-                    <div className='comment-product-name'>
-                        <Link to={`/product-detail/${product}`} target="_blank" rel="noopener noreferrer" >{productName}</Link>
-                    </div> : <></>
-                }
-                <div className="row-space-between">
-                    <div className="comment-author">
-                        {name}
-                    </div>
-                    <div className="comment-date">{new Date(updatedAt).toLocaleString()}</div>
-                </div>
-                <div className="row star">
-                    {
-                        [1, 2, 3, 4, 5].map((item, index) => {
-                            if (item <= rating) {
-                                return <p key={index} className="star checked"><i class="fas fa-star"></i></p>
-                            } else {
-                                return <p key={index} className="star"><i class="fas fa-star"></i></p>
-                            }
-                        })
-                    }
-                </div>
-                <div className="comment-content">
-                    {comment}
-                </div>
-            </div>
+  return (
+    <div className="comment">
+      <div className="comment__item">
+        {product ? (
+          <div className="comment-product-name">
+            <Link
+              to={`/product-detail/${product}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {productName}
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="row-space-between">
+          <div className="comment-author">{name}</div>
+          <div className="comment-date">
+            {new Date(updatedAt).toLocaleString()}
+          </div>
         </div>
-    )
+        <div className="row justify-space">
+          <div className="row star">
+            {[1, 2, 3, 4, 5].map((item, index) => {
+              if (item <= rating) {
+                return (
+                  <p key={index} className="star checked">
+                    <i class="fas fa-star"></i>
+                  </p>
+                );
+              } else {
+                return (
+                  <p key={index} className="star">
+                    <i class="fas fa-star"></i>
+                  </p>
+                );
+              }
+            })}
+          </div>
+          {isPurchase && (
+            <div className="row comment-is-purchase star">
+              {/* <i class="fas fa-badge-check"></i> */}
+              <i class="fas fa-user-check"></i>
+              <p>Đã mua</p>
+            </div>
+          )}
+        </div>
+        <div className="comment-content">{comment}</div>
+      </div>
+    </div>
+  );
 }
--- src/pages/ProductDetail/item/Review/index.js
+++ src/pages/ProductDetail/item/Review/index.js
@@ -1,67 +1,97 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
-import Comment from '../../../../components/Comment';
-import './style.scss';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import Comment from "../../../../components/Comment";
+import "./style.scss";
 export default function Review(props) {
-    const { reviews } = props
-    const { userInfo } = useSelector(state => state.userLogin);
-    const [ratingNumber, setRatingNumber] = useState(5);
-    const { pathname } = useLocation();
-    console.log(reviews);
-    function onChangeRating(number) {
-        setRatingNumber(number);
+  const { reviews } = props;
+  const { userInfo } = useSelector((state) => state.userLogin);
+  const [ratingNumber, setRatingNumber] = useState(5);
+  const { pathname } = useLocation();
+  console.log(reviews);
+  function onChangeRating(number) {
+    setRatingNumber(number);
+  }
+  const [comment, setComment] = useState("");
+  function onChangeComment(e) {
+    setComment(e.target.value);
+  }
+  function sendReview() {
+    if (comment !== "") {
+      const review = {
+        comment: comment,
+        rating: ratingNumber,
+      };
+      props.createReview(review);
     }
-    const [comment, setComment] = useState('')
-    function onChangeComment(e) {
-        setComment(e.target.value);
-    }
-    function sendReview() {
-        if (comment !== '') {
-            const review = {
-                comment: comment,
-                rating: ratingNumber,
-            }
-            props.createReview(review)
-        }
-    }
-    return (
-        <div className="row shadow">
-            <div className="col c-6 md-12 review">
-                <h3 className="review-title">Đánh Giá - Nhận Xét Từ Khách Hàng
-                </h3>
-                {(reviews !== undefined && reviews.length > 0) ? reviews.map((item, index) => <Comment key={index} review={item} />) : <div className="row center-item review-none">Chưa có nhận xét nào</div>}
+  }
+  return (
+    <div className="row shadow">
+      <div className="col c-6 md-12 review">
+        <h3 className="review-title">Đánh Giá - Nhận Xét Từ Khách Hàng</h3>
+        {reviews !== undefined && reviews.length > 0 ? (
+          reviews.map((item, index) => <Comment key={index} review={item} />)
+        ) : (
+          <div className="row center-item review-none">
+            Chưa có nhận xét nào
+          </div>
+        )}
+      </div>
+      <div className="col c-6 md-12 write-review">
+        <h3 className="review-title">Thêm đánh giá</h3>
+        {userInfo?.email ? (
+          <>
+            <div className="row rating member">
+              <p className="title-text">Đánh giá: </p>
+              {[1, 2, 3, 4, 5].map((item) => {
+                if (item <= ratingNumber) {
+                  return (
+                    <p
+                      className="star checked"
+                      onClick={() => onChangeRating(item)}
+                    >
+                      <i class="fas fa-star"></i>
+                    </p>
+                  );
+                } else {
+                  return (
+                    <p className="star" onClick={() => onChangeRating(item)}>
+                      <i class="fas fa-star"></i>
+                    </p>
+                  );
+                }
+              })}
             </div>
-            <div className="col c-6 md-12 write-review">
-                <h3 className="review-title">Thêm đánh giá</h3>
-                {userInfo?.email ? <>
-                    <div className="row rating member">
-                        <p className="title-text">Đánh giá: </p>
-                        {
-                            [1, 2, 3, 4, 5].map((item) => {
-                                if (item <= ratingNumber) {
-                                    return <p className="star checked" onClick={() => onChangeRating(item)}><i class="fas fa-star"></i></p>
-                                } else {
-                                    return <p className="star" onClick={() => onChangeRating(item)}><i class="fas fa-star"></i></p>
-                                }
-                            })
-                        }
-                    </div>
-                    <p><span className="title-text">Nhận xét:</span><textarea type="text" onChange={onChangeComment} /></p>
-                    <button className="btn btn--border-none" onClick={sendReview}>Gửi đánh giá {props.loading ? "..." : ""}</button></> :
-                    <div className="client">
-                        <p className="title">Đăng nhập để gửi nhận xét của bạn</p>
-                        <div className="row">
-                            <div className="c-6 lg-12 md-12">
-                                <Link className="btn client-login" to={`/login?redirect=${pathname}`}>Đăng nhập</Link>
-                            </div>
-                            <div className="c-6 lg-12 md-12">
-                                <p className="sub-text">Bạn chưa có tài khoản?</p>
-                                <Link to="/register" className="link link--underline">Đăng ký</Link>
-                            </div>
-                        </div>
-                    </div>}
+            <p>
+              <span className="title-text">Nhận xét:</span>
+              <textarea type="text" onChange={onChangeComment} />
+            </p>
+            <button className="btn btn--border-none" onClick={sendReview}>
+              Gửi đánh giá {props.loading ? "..." : ""}
+            </button>
+          </>
+        ) : (
+          <div className="client">
+            <p className="title">Đăng nhập để gửi nhận xét của bạn</p>
+            <div className="row">
+              <div className="c-6 lg-12 md-12">
+                <Link
+                  className="btn client-login"
+                  to={`/login?redirect=${pathname}`}
+                >
+                  Đăng nhập
+                </Link>
+              </div>
+              <div className="c-6 lg-12 md-12">
+                <p className="sub-text">Bạn chưa có tài khoản?</p>
+                <Link to="/register" className="link link--underline">
+                  Đăng ký
+                </Link>
+              </div>
             </div>
-        </div >
-    )
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
