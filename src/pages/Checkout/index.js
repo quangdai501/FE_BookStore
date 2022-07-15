13f81663b21637149c1631e433a75eff40a95ca9
Nguyen Van Linh <linh.nguyen04@hastech.vn>
2022-07-15 11:31:36 +0700

    Update index.js

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect, useCallback } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { createOrder, sendMailOrder } from '../../actions/orderAction';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createOrder, sendMailOrder } from "../../actions/orderAction";
 import { priceToString } from "../../common/convertNumberToPrice";
 import Item from "./item";
 import { CART_CLEAR_ITEMS } from "../../constants/cart";
@@ -12,14 +12,16 @@ const Checkout = () => {
   const [provinces, setProvinces] = useState([]);
   const [districts, setDistricts] = useState([]);
   const [wards, setWards] = useState([]);
-  const orderInfo = useSelector(state => state.createOrder);
+  const [currentDistrict, setCurrentDistrict] = useState(1452);
+  const [currentWardCode, setCurrentWardCode] = useState("21012");
+  const [shippingFee, setShippingFee] = useState(15000);
+  const orderInfo = useSelector((state) => state.createOrder);
   const cart = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.userLogin);
-  const { sendingProcess } = useSelector(state => state.sendMailOrder);
+  const { sendingProcess } = useSelector((state) => state.sendMailOrder);
   const { createOrderProcess } = orderInfo;
   const { cartItems } = cart;
   const navigate = useNavigate();
-  const shippingFee = 15000;
   const {
     register,
     handleSubmit,
@@ -28,69 +30,144 @@ const Checkout = () => {
   } = useForm();
 
   useEffect(() => {
-    setValue("name", userInfo?.name)
-    setValue("phone", userInfo?.phone)
-  }, [])
+    setValue("name", userInfo?.name);
+    setValue("phone", userInfo?.phone);
+  }, []);
   const getProvince = async () => {
-    const { data: { data } } = await axios.get(`https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province`, {
-      headers: {
-        token: "ef8f9c13-2315-11ec-b8c6-fade198b4859",
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province`,
+      {
+        headers: {
+          token: "ef8f9c13-2315-11ec-b8c6-fade198b4859",
+        },
       }
-    })
+    );
 
-    setProvinces(data.reduce((list, item) => {
-      list.push({
-        ProvinceID: item.ProvinceID,
-        ProvinceName: item.ProvinceName
-      });
-      return list.sort((first, second) => first.ProvinceID - second.ProvinceID);
-    }, []));
-  }
+    setProvinces(
+      data.reduce((list, item) => {
+        list.push({
+          ProvinceID: item.ProvinceID,
+          ProvinceName: item.ProvinceName,
+        });
+        return list.sort(
+          (first, second) => first.ProvinceID - second.ProvinceID
+        );
+      }, [])
+    );
+  };
   const getDistrict = async (ProvinceID) => {
-    const { data: { data } } = await axios.get(`https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/district?province_id=${ProvinceID}`, {
-      headers: {
-        token: "ef8f9c13-2315-11ec-b8c6-fade198b4859",
-      }
-    })
-    setDistricts(data);
-  }
+    try {
+      const {
+        data: { data },
+      } = await axios.get(
+        `https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/district?province_id=${ProvinceID}`,
+        {
+          headers: {
+            token: "ef8f9c13-2315-11ec-b8c6-fade198b4859",
+          },
+        }
+      );
+      setDistricts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const getWard = async (districtID) => {
-    const { data: { data } } = await axios.get(`https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/ward?district_id=${districtID}`, {
-      headers: {
-        token: "ef8f9c13-2315-11ec-b8c6-fade198b4859",
+    try {
+      const {
+        data: { data },
+      } = await axios.get(
+        `https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/ward?district_id=${districtID}`,
+        {
+          headers: {
+            token: "ef8f9c13-2315-11ec-b8c6-fade198b4859",
+          },
+        }
+      );
+      setWards(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getShippingFee = useCallback(
+   async () => {
+      const body = {
+        from_district_id: 1459,
+        service_id: 53320,
+        service_type_id: null,
+        to_district_id: currentDistrict,
+        to_ward_code: currentWardCode,
+        height: cartItems.length,
+        length: 20,
+        weight: 100 * cartItems.length,
+        width: 10,
+        insurance_value: 10000,
+        coupon: null,
+      };
+      try {
+        const {
+          data: { data },
+        } = await axios.post(
+          `https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee`,
+          body,
+          {
+            headers: {
+              token: "e4822a9f-4eba-11ec-ac64-422c37c6de1b",
+              ShopId: "83990",
+            },
+          }
+        );
+
+        setShippingFee(data.total);
+      } catch (e) {
+        console.log(e);
       }
-    })
-    setWards(data);
-  }
+    },
+    [currentDistrict, currentWardCode]
+  );
 
   const onChangeProvince = (e) => {
     const name = e.target.value;
-    const province = provinces.find(item => item.ProvinceName === name);
+    const province = provinces.find((item) => item.ProvinceName === name);
     getDistrict(province.ProvinceID);
-  }
+  };
 
   const onChangeDistrict = (e) => {
     const name = e.target.value;
-    const district = districts.find(item => item.DistrictName === name);
+    const district = districts.find((item) => item.DistrictName === name);
     getWard(district.DistrictID);
-  }
-
+    setCurrentDistrict(district.DistrictID);
+  };
+  const onChangeWard = (e) => {
+    const name = e.target.value;
+    const ward = wards.find((item) => item.WardName === name);
+    setCurrentWardCode(ward?.WardCode);
+  };
   const dispatch = useDispatch();
 
-  const totalCart = useMemo(() =>
-    cartItems.reduce((total, item) => total += item.price * item.qty, 0), [cartItems]);
+  const totalCart = useMemo(
+    () =>
+      cartItems.reduce((total, item) => (total += item.price * item.qty), 0),
+    [cartItems]
+  );
 
-  const billDetail = useMemo(() =>
-    cartItems.reduce((list, currItem) => {
-      list.push({
-        productId: currItem.product,
-        name: currItem.name,
-        image: currItem.image,
-        price: currItem.price,
-        qty: currItem.qty,
-      })
-      return list
-    }, []), [cartItems]);
+  const billDetail = useMemo(
+    () =>
+      cartItems.reduce((list, currItem) => {
+        list.push({
+          productId: currItem.product,
+          name: currItem.name,
+          image: currItem.image,
+          price: currItem.price,
+          qty: currItem.qty,
+        });
+        return list;
+      }, []),
+    [cartItems]
+  );
 
   const phonePatterm = {
     value: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
@@ -101,30 +178,64 @@ const Checkout = () => {
     console.log(data);
     if (data) {
       const address = {
-        to_ward_code: Number(wards.find(item => item.WardName === data.village).WardCode),
-        to_district_id: Number(districts.find(item => item.DistrictName === data.district).DistrictID),
+        to_ward_code: Number(
+          wards.find((item) => item.WardName === data.village).WardCode
+        ),
+        to_district_id: Number(
+          districts.find((item) => item.DistrictName === data.district)
+            .DistrictID
+        ),
         province: data.province,
         district: data.district,
         ward: data.village,
         detail: data.address,
-      }
+      };
       if (data.payment === "online") {
-        await dispatch(createOrder(userInfo._id, data.name, totalCart + shippingFee, address, data.phone, billDetail, "Thanh toán online"));
-      }
-      else {
-        await dispatch(createOrder(userInfo._id, data.name, totalCart + shippingFee, address, data.phone, billDetail, "Thanh toán khi nhận hàng", navigate));
+        await dispatch(
+          createOrder(
+            userInfo._id,
+            data.name,
+            totalCart + shippingFee,
+            address,
+            data.phone,
+            billDetail,
+            "Thanh toán online"
+          )
+        );
+      } else {
+        await dispatch(
+          createOrder(
+            userInfo._id,
+            data.name,
+            totalCart + shippingFee,
+            address,
+            data.phone,
+            billDetail,
+            "Thanh toán khi nhận hàng",
+            navigate
+          )
+        );
         await dispatch(sendMailOrder(userInfo, cartItems));
         await dispatch({ type: CART_CLEAR_ITEMS });
       }
     }
   };
   useEffect(() => {
-    getProvince()
-  }, [])
+    getProvince();
+  }, []);
+  useEffect(() => {
+    if (currentDistrict && currentWardCode) {
+      getShippingFee();
+    }
+  }, [currentDistrict, currentWardCode]);
+
   return (
     <div className="checkout space">
       <h1>Thanh toán</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="row gutter checkout-session">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="row gutter checkout-session"
+      >
         <div className="c-6 lg-6 md-12 padding center-item">
           <div className="Billing-details checkout-info">
             <h3 className="title mb-15 text-center">Thông tin khách hàng</h3>
@@ -145,7 +256,10 @@ const Checkout = () => {
               <input
                 type="text"
                 name="phone"
-                {...register("phone", { required: true, pattern: phonePatterm })}
+                {...register("phone", {
+                  required: true,
+                  pattern: phonePatterm,
+                })}
               />
               {errors.phone && (
                 <p className="error-label">{errors.phone.message}</p>
@@ -155,10 +269,18 @@ const Checkout = () => {
               <label htmlFor="province" className="form-label">
                 Tỉnh, Thành phố
               </label>
-              <select name="province" {...register("province", { required: true })} onChange={onChangeProvince}>
-                <option defaultValue value="">Chọn Tỉnh, Thành phố</option>
+              <select
+                name="province"
+                {...register("province", { required: true })}
+                onChange={onChangeProvince}
+              >
+                <option defaultValue value="">
+                  Chọn Tỉnh, Thành phố
+                </option>
                 {provinces?.map((item) => (
-                  <option value={item.ProvinceName} key={item.ProvinceID}>{item.ProvinceName}</option>
+                  <option value={item.ProvinceName} key={item.ProvinceID}>
+                    {item.ProvinceName}
+                  </option>
                 ))}
               </select>
             </div>
@@ -166,10 +288,18 @@ const Checkout = () => {
               <label htmlFor="district" className="form-label">
                 Quận, Huyện
               </label>
-              <select name="district" {...register("district", { required: true })} onChange={onChangeDistrict}>
-                <option defaultValue value="">Chọn Quận, Huyện</option>
+              <select
+                name="district"
+                {...register("district", { required: true })}
+                onChange={onChangeDistrict}
+              >
+                <option defaultValue value="">
+                  Chọn Quận, Huyện
+                </option>
                 {districts.map((item) => (
-                  <option value={item.DistrictName} key={item.DistrictID}>{item.DistrictName}</option>
+                  <option value={item.DistrictName} key={item.DistrictID}>
+                    {item.DistrictName}
+                  </option>
                 ))}
               </select>
             </div>
@@ -177,10 +307,18 @@ const Checkout = () => {
               <label htmlFor="village" className="form-label">
                 Xã, Phường
               </label>
-              <select name="village" {...register("village", { required: true })}>
-                <option defaultValue value="">Chọn Xã, Phường</option>
+              <select
+                name="village"
+                {...register("village", { required: true })}
+                onChange={onChangeWard}
+              >
+                <option defaultValue value="">
+                  Chọn Xã, Phường
+                </option>
                 {wards.map((item) => (
-                  <option value={item.WardName} key={item.WardCode}>{item.WardName}</option>
+                  <option value={item.WardName} key={item.WardCode}>
+                    {item.WardName}
+                  </option>
                 ))}
               </select>
             </div>
@@ -200,9 +338,11 @@ const Checkout = () => {
           <div className="Orders shadow">
             <h3 className="title mb-15 text-center">Sản phẩm của bạn</h3>
             <div className="order-row">
-              {cartItems ? cartItems.map((item, index) => (
-                <Item cart={item} key={index} />
-              )) : <></>}
+              {cartItems ? (
+                cartItems.map((item, index) => <Item cart={item} key={index} />)
+              ) : (
+                <></>
+              )}
             </div>
             <div className="order-row">
               <div className="row">
@@ -213,13 +353,17 @@ const Checkout = () => {
             <div className="order-row">
               <div className="row">
                 <p className=" col c-8 md-12">Chi phí vận chuyển:</p>
-                <div className="col c-4 md-12">{priceToString(shippingFee)}</div>
+                <div className="col c-4 md-12">
+                  {priceToString(shippingFee)}
+                </div>
               </div>
             </div>
             <div className="order-row">
               <div className="row">
                 <h3 className=" col c-8 md-6">Tổng: </h3>
-                <h3 className=" col c-4 md-6">{priceToString(shippingFee + totalCart)}</h3>
+                <h3 className=" col c-4 md-6">
+                  {priceToString(shippingFee + totalCart)}
+                </h3>
               </div>
             </div>
             <div className="order-row">
@@ -232,7 +376,9 @@ const Checkout = () => {
                   name="payment"
                   {...register("payment")}
                 />
-                <label htmlFor="payment text-sm">Thanh toán bằng tiền mặt</label>
+                <label htmlFor="payment text-sm">
+                  Thanh toán bằng tiền mặt
+                </label>
               </div>
               <div className="row sub-title">
                 <input
@@ -241,7 +387,9 @@ const Checkout = () => {
                   value="online"
                   {...register("payment")}
                 />
-                <label htmlFor="payment text-sm">Thanh toán bằng ví điện tử vnpay</label>
+                <label htmlFor="payment text-sm">
+                  Thanh toán bằng ví điện tử vnpay
+                </label>
                 <img className="payment-logo" src="./images/vnpay.png" alt="" />
               </div>
             </div>
@@ -250,7 +398,9 @@ const Checkout = () => {
                 className="btn btn--border-none btn--full-width"
                 type="submit"
               >
-                {createOrderProcess || sendingProcess ? "Đang xử lý..." : "Đặt hàng"}
+                {createOrderProcess || sendingProcess
+                  ? "Đang xử lý..."
+                  : "Đặt hàng"}
               </button>
             </div>
           </div>
